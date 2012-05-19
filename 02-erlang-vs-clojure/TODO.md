c2c2983af5a850f452d1bb01e60993eff0cf0189
Lars Hesel Christensen <larshesel@gmail.com>
2012-05-19 12:12:22 +0200

    add some pattern matching code and added some stuff to todo

--- 02-erlang-vs-clojure/TODO.md
+++ 02-erlang-vs-clojure/TODO.md
@@ -1,4 +1,16 @@
 # TODOs
 
 1. Erlang can sort all sorts of items - Clojure needs to reify types
-2. 
+2. type system
+producer-consumer
+
+lazy-eval (Erlang har strict eval)
+
+telegram-parsning (fx, tcp pakker)
+
+Pattern matching
+
+
+modularisering
+
+datastrukturer - trae/graf/... 
--- 02-erlang-vs-clojure/erlang/functions.erl
+++ 02-erlang-vs-clojure/erlang/functions.erl
@@ -1,12 +1,28 @@
 -module(functions).
 
--export([numbers/1]).
+-compile(export_all).
 
 numbers(1) -> one;
 numbers(2) -> two;
 numbers(X) when X > 2 ->
     many.
 
+
+numbers_case(X) ->
+    case X of 
+	1 -> one;
+	2 -> two;
+	_ -> many
+    end.
+
+
+deconstruction([_,[_,{_,What}]]) ->
+	What. 
+
+%% (emacs@stella)11> functions:deconstruction([{1, banan}, [liste, {1, tupel}]]).
+%% tupel
+
+	
 
 
 
